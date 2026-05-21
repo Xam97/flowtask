149db49ce911df0cc7ed5f6cbba3816a468bf056
ikrachill <ikracrack87@gmail.com>
2026-05-20 23:34:11 -0300

    chore: registro de configuración de la app boards en Django

--- flowtask/boards/apps.py
+++ flowtask/boards/apps.py
@@ -1,6 +1,31 @@
+# users/apps.py
 from django.apps import AppConfig
 
+class UsersConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'users'
+    verbose_name = 'Usuarios y Autenticación'
 
+# boards/apps.py
 class BoardsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'boards'
+    verbose_name = 'Tableros y Tarjetas'
+
+# notifications/apps.py
+class NotificationsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'notifications'
+    verbose_name = 'Notificaciones'
+
+# activity/apps.py
+class ActivityConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'activity'
+    verbose_name = 'Actividad Reciente'
+
+# websockets/apps.py
+class WebsocketsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'websockets'
+    verbose_name = 'WebSockets'
